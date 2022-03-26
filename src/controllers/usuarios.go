03c353fdc8d998c10efc2502c3845132caa7581b
lucio <you@example.com>
2022-03-25 22:01:35 -0300

    Primeira comunicaçao do Front-end com a API do Back-end

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,36 +1,35 @@
 package controllers
 
 import (
-	// "bytes"
-	// "encoding/json"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	// "log"
+	"log"
 	"net/http"
 )
 
 //CriarUsuario chama a API para cadastrar um usuário no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()  //Fazer a função do postman
+	r.ParseForm() //Fazer a função do postman
 
-	nome := r.FormValue("nome")
-	fmt.Println(nome)
+	usuario, erro := json.Marshal(map[string]string{
+		"nome":  r.FormValue("nome"),
+		"email": r.FormValue("email"),
+		"nick":  r.FormValue("nick"),
+		"senha": r.FormValue("senha"),
+	})
 
-	// usuario, erro := json.Marshal(map[string]string{
-	// 	"nome": r.FormValue("nome"),
-	// 	"email": r.FormValue("email"),
-	// 	"nick": r.FormValue("nick"),
-	// 	"senha": r.FormValue("senha"),
-	// })
-
-	// if erro != nil {
-	// 	log.Fatal(erro)
-	// }
-
-	// response, erro := http.Post("http://localhost:5000/usuarios", "application/json", bytes.NewBuffer(usuario))
-	// if erro != nil {
-	// 	log.Fatal(erro)
-	// }
-	// defer response.Body.Close()
-
-	// fmt.Println(response.Body)
+		if erro != nil {
+		log.Fatal(erro)
+	}
+	
+	//COMUNICAÇÃO DO FRONT-END COM O BACK-END////// NewBuffer tranforma de slice de byte para json
+	response, erro := http.Post("http://localhost:5000/usuarios", "application/json", bytes.NewBuffer(usuario))
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer response.Body.Close()
+	
+	fmt.Println(response.Body)
 }
+
