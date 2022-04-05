c41633072e0611dc486469c1a61d9c09cf1d627e
lucio <you@example.com>
2022-04-04 22:17:06 -0300

    Implementaçao de icones por Font Awesome e inicio da funçao de curtir publicaçao

--- assets/js/publicacoes.js
+++ assets/js/publicacoes.js
@@ -1,19 +1,31 @@
-$('#nova-publicacao').on('submit', criarPublicacao);
+$('#nova-publicacao').on('submit', criarPublicacao) // # é para id
+$('.curtir-publicacao').on('click', curtirPublicacao) // . é para classe
+
 
 function criarPublicacao(evento) {
-   evento.preventDefault();
+  evento.preventDefault()
 
-   $.ajax({
-     url: "/publicacoes",
-     method: "POST",
-     data: {
-       titulo: $('#titulo').val(),
-       conteudo: $('#conteudo').val(),
-     }
-  }).done(function() {
-    window.location = "/home";
-  }).fail(function() {
-    alert("Erro ao criar a publicação");
+  $.ajax({
+    url: '/publicacoes',
+    method: 'POST',
+    data: {
+      titulo: $('#titulo').val(),
+      conteudo: $('#conteudo').val()
+    }
   })
+    .done(function () {
+      window.location = '/home'
+    })
+    .fail(function () {
+      alert('Erro ao criar a publicação')
+    })
+}
 
+function curtirPublicacao(evento) {
+  evento.preventDefault();
+
+  const elementoClicado = $(evento.target);
+  const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+  console.log(publicacaoId);
 }
