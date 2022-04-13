671261d3b9afaf54cbbd633740b631484b468af1
Lúcio do Nascimento <luciomate95@gmail.com>
2022-04-12 22:56:46 -0300

    Validaçao para usuario logado

--- src/controllers/paginas.go
+++ src/controllers/paginas.go
@@ -19,7 +19,12 @@ import (
 
 // CarregarTelaDeLogin renderiza a tela de login
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+  cookie, _ := cookies.Ler(r)
 
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", 302)
+		return
+	}
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
