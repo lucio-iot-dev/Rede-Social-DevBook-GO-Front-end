d4d7c7bfa1dab0685ba680a346fd4fac8d1158fb
Lúcio do Nascimento <luciomate95@gmail.com>
2022-04-15 11:24:08 -0300

    Perfil do usuario logado

--- src/router/rotas/usuarios.go
+++ src/router/rotas/usuarios.go
@@ -42,4 +42,10 @@ var rotasUsuarios = []Rota{
 		Funcao:             controllers.SeguirUsuario,
 		RequerAutenticacao: true,
 	},
+	{
+		URI:                "/perfil",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.CarregarPerfilDoUsuarioLogado,
+		RequerAutenticacao: true,
+	},
 }
